d2a7757988ecef39760beef27fe3bd7df95846eb
JonasHope <95291501+JonasHope@users.noreply.github.com>
2023-01-23 23:57:49 +0100

    testing unit-test on login

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -1,1 +1,38 @@
 import { login } from "./login";
+
+const loginMock = jest.fn().mockResolvedValue({
+  ok: true,
+  json: jest.fn().mockResolvedValue({
+    name: "test",
+    email: "test@noroff.no",
+    banner: "",
+    avatar: "",
+    token: "token",
+  }),
+});
+
+global.fetch = loginMock;
+class LSMock {
+  constructor() {
+    this.storage = {};
+  }
+  clear() {
+    this.storage = {};
+  }
+  getItem(key) {
+    return this.storage[key] || "";
+  }
+  setItem(key, value) {
+    this.storage[key] = String(value);
+  }
+}
+
+global.localStorage = new LSMock();
+describe("login", () => {
+  it("saves token when successfull login", async () => {
+    localStorage.clear();
+    await login("test@noroff.no", "");
+    const token = localStorage.getItem("token");
+    expect(token).not.toBeNull();
+  });
+});
